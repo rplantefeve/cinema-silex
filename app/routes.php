b98f87d62c5acb7b94c753c8fd45a7b8e71eb85f
Patrice62 <patricedu62@sfr.fr>
2016-12-19 12:26:59 +0100

    Cinemalist

--- app/routes.php
+++ app/routes.php
@@ -9,3 +9,19 @@ $app->get('/home',
 'Semeformation\\Mvc\\Cinema_crud\\controllers\\HomeController::home')
 ->bind('home');
 
+// CinemaList, editCinema, delete cinema
+$app->get('/cinemaList',
+'Semeformation\\Mvc\\Cinema_crud\\controllers\\CinemaController::cinemaList')
+->bind('cinema/list');
+
+$app->get('/editCinema',
+'Semeformation\\Mvc\\Cinema_crud\\controllers\\CinemaController::editCinema')
+->bind('cinema/edit/cinemaID');
+
+$app->get('/editCinema',
+'Semeformation\\Mvc\\Cinema_crud\\controllers\\CinemaController::editCinema/add')
+->bind('cinema/add');
+
+$app->get('/cinemaDelete',
+'Semeformation\\Mvc\\Cinema_crud\\controllers\\CinemaController::cinemaDelete')
+->bind('cinema/cinemaDelete/cinemaID');
